9200307e8951e732e44c5dc8f724b440ab917bc2
Stephen Palmer <stephenp@unity3d.com>
2019-01-14 09:28:55 -0600

    Add tests for stream recorder

---
+++ test/client_stream_recorder.js
@@ -0,0 +1,102 @@
+require('./test_init');
+
+const assert = require('assert');
+const sinon = require('sinon');
+const uuid = require('uuid');
+const path = require('path');
+const tmp = require('tmp');
+const fs = require('fs-extra');
+const {promisify} = require('util');
+const {Readable} = require('stream');
+const consts = require('../lib/constants');
+const {encodeCommand, cmd, randomBuffer} = require('./test_utils');
+const ClientStreamRecorder = require('../lib/server/client_stream_recorder');
+
+
+describe('ClientStreamRecorder', () => {
+
+    before(() => {
+        this.tmpDir = tmp.dirSync({unsafeCleanup: true});
+    });
+
+    after(() => {
+        this.tmpDir.removeCallback();
+    });
+
+    it('should set default for missing options', () => {
+        const csr = new ClientStreamRecorder({});
+        assert(csr._sessionId);
+        assert(csr._saveDir);
+        assert(csr._bufferSize);
+    });
+
+    it('should use options passed into constructor', () => {
+        const opts = {
+            sessionId: uuid.v4(),
+            saveDir: 'temp',
+            bufferSize: Math.floor(Math.random() * 100000)
+        };
+
+        const csr = new ClientStreamRecorder(opts);
+        assert.strictEqual(csr._sessionId, opts.sessionId);
+        assert.strictEqual(csr._saveDir, opts.saveDir);
+        assert.strictEqual(csr._bufferSize, opts.bufferSize);
+    });
+
+    it('should not write to disk until the internal buffer is full', async () => {
+        const opts = {
+            saveDir: this.tmpDir.name,
+            bufferSize: 1024
+        };
+
+        const csr = new ClientStreamRecorder(opts);
+        const csrWrite = promisify(csr.write).bind(csr);
+        const dataPath = csr.dataPath;
+
+        await csrWrite(Buffer.alloc(1023));
+        assert(!await fs.pathExists(dataPath));
+        await csrWrite(Buffer.alloc(2));
+        assert(await fs.pathExists(dataPath));
+    });
+
+    it('should write the command buffer to disk with a normalized protocol version', async () => {
+        const opts = {
+            saveDir: this.tmpDir.name,
+            bufferSize: 64
+        };
+
+        const csr = new ClientStreamRecorder(opts);
+
+        const guid = randomBuffer(consts.GUID_SIZE);
+        const hash = randomBuffer(consts.HASH_SIZE);
+
+        const buffer = Buffer.from('fe' +
+            encodeCommand(cmd.getAsset, guid, hash) +
+            encodeCommand(cmd.getInfo, guid, hash) +
+            encodeCommand(cmd.getResource, guid, hash), 'ascii');
+
+        const reader = new Readable({
+            read() {
+                this.push(buffer);
+                this.push(null);
+            }
+        });
+
+        reader.pipe(csr);
+
+        await new Promise(resolve => {
+            reader.on('end', () => {
+                reader.unpipe();
+                resolve();
+            });
+        });
+
+        await new Promise(resolve => {
+            csr.on('flushed', () => resolve());
+        });
+
+        assert(await fs.pathExists(csr.dataPath));
+        const fileData = await fs.readFile(csr.dataPath);
+        assert.equal(Buffer.compare(buffer.slice(2), fileData.slice(consts.VERSION_SIZE)), 0);
+    });
+});
